0998779c95aa04268f70f027c66d8850fc710417
Dmitry Guskov <guskovd@internet.ru>
2025-12-28 13:27:32 +0300

    Create stats module (client, dto, server)

---
+++ stats/stats-server/src/main/java/ru/practicum/stats_server/controller/StatsController.java
@@ -0,0 +1,35 @@
+package ru.practicum.stats_server.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.dto.HitDto;
+import ru.practicum.dto.ResponseStatsDto;
+import ru.practicum.stats_server.exception.ValidationException;
+import ru.practicum.stats_server.service.HitService;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class StatsController {
+    private final HitService service;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/hit")
+    public void saveHit(@RequestBody @Valid HitDto hitDto) {
+        service.save(hitDto);
+    }
+
+    @GetMapping("/stats")
+    public List<ResponseStatsDto> findStats(@RequestParam LocalDateTime start, @RequestParam LocalDateTime end,
+                                            @RequestParam(required = false) List<String> uris,
+                                            @RequestParam(required = false, defaultValue = "false") boolean unique) {
+        if (start.isAfter(end))
+            throw new ValidationException("Дата начала диапазона не может быть позже даты конца");
+
+        return service.find(start, end, uris, unique);
+    }
+}
